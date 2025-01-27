90a58a2420496a64306c3c29859dcd1b5c4ba4e9
Rafael Aznar <rafaaznar@gmail.com>
2025-01-27 17:28:23 +0100

    interceptor funcionando

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { JWTInterceptor } from './interceptor/jwt.interceptor';
-import { SessionService } from './service/session.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(),
-    //{ provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true }
+    provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(),
     provideHttpClient(
-      withInterceptors([
-        new JWTInterceptor(new SessionService())
-      ])
-    )
+      withInterceptorsFromDi()
+    ),
+    { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true }
   ]
 };
--- src/app/interceptor/jwt.interceptor.ts
+++ src/app/interceptor/jwt.interceptor.ts
@@ -1,15 +1,14 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
-import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { SessionService } from "../service/session.service";
 
 @Injectable()
-export class JWTInterceptor implements HttpInterceptorFn {
+export class JWTInterceptor implements HttpInterceptor {
 
     constructor(private oSessionService: SessionService) { }
 
-    intercept(req: HttpRequest<any>, next: HttpInterceptorFn): Observable<HttpEvent<any>> {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         if (this.oSessionService.isSessionActive()) {
             const token = this.oSessionService.getToken();
